d171607679f8ccdfd0633473e6b1a4f2afce6e61
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-02-14 05:50:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,12 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     if BRANDS.include?(brand) == false 
-      BRANDS << brand
-  end
-  
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
+      BRANDS << @brand
   end
   
 end
